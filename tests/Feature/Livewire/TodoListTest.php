c5fcd75ed55ae2d9321c725ed251fc84196e84e9
juanc <juancruzliberati@hotmail.com>
2025-08-05 02:50:52 -0300

    Parte 4 Testing de componentes de livewire 3 con php pest

--- tests/Feature/Livewire/TodoListTest.php
+++ tests/Feature/Livewire/TodoListTest.php
@@ -11,8 +11,14 @@ use App\Livewire\Todos\TodoList;
 use App\Models\User;
 
 test('todo list component renders',function (){
+    // primero debemos estar logeados, creamos un usuario con Livewire::actingAs
     Livewire::actingAs(User::factory()->create())
         ->test(TodoList::class)
         ->assertSee('No hay tareas a realizar');
-});
+
+    // segundo, en el metodo test(), indicamos el componente a testear
+    //tercero, en el metodo  assertSee(), indicamos la informacion a validar, debe existir
+})->group('todo-list','todos');
+
+// agrupacion de metodos --> todo-list
 
