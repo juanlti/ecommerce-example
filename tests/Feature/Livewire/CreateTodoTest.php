04aede2068adf40a3a541e9321e839a387b77e3b
juanc <juancruzliberati@hotmail.com>
2025-08-05 23:13:49 -0300

    Parte 4 Testing todo create component renders

--- tests/Feature/Livewire/CreateTodoTest.php
+++ tests/Feature/Livewire/CreateTodoTest.php
@@ -11,7 +11,7 @@ test('example', function () {
 use App\Livewire\Todos\CreateTodo;
 use App\Models\User;
 
-test('create todo  component renders', function () {
+test('view create todo  component renders', function () {
 
     $user = User::factory()->create([
         'name' => 'Juan',
@@ -26,3 +26,31 @@ test('create todo  component renders', function () {
 
 
 })->group('create-todo', 'todos');
+
+
+test('create todo  component renders', function () {
+
+    $user = User::factory()->create([
+        'name' => 'Juan',
+        'email' => 'delete@prueba.com'
+    ]);
+    $action = Livewire::actingAs($user);
+    $action->test(CreateTodo::class)
+        ->set('form.title', 'comprar pan')
+        ->set('form.description', 'ir a la panaderia y comprar pan')
+        ->assertSee('Completada')
+        ->set('form.done', false)
+        ->assertSee('Crear')
+        ->call('save')
+        ->assertRedirect(route('todos.index'));
+    //verificamos que le dato se encuentre efectivamente en la bd
+    $this->assertDatabaseHas('todos', [
+        'title' => 'comprar pan',
+        'description' => 'ir a la panaderia y comprar pan',
+        'done' => false
+    ]);
+
+
+})->group('create-todo', 'todos');
+
+
